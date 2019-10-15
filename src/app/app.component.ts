bf171e1c4fd80e39ffc9a918a37f4d84259377ea
sakhamuri92 <sakhamurivenkatesh@gmail.com>
2019-10-15 21:16:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker'
+import { DataService } from './data.service';
+import { convertUpdateArguments } from '@angular/compiler/src/compiler_util/expression_converter';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularpwa';
+  joke:any;
+  update = false
+  constructor(private updates:SwUpdate,private dataService:DataService){
+    this.updates.available.subscribe(event =>{
+      // this.update = true
+      updates.activateUpdate().then(() => document.location.reload())
+    })  
+  }
+  ngOnInit() {
+    this.dataService.getResponse().subscribe((joke) =>{
+      this.joke = joke
+    })
+  }
+  subscribeToPush() {
+    // Notification.requestPermission(permission =>{
+    //   if(permission == "granted") {
+    //   }
+    // })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
